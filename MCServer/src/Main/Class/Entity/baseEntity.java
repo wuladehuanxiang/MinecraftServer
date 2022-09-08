ad78b1590f02b20363d1c32d30bdc3bd313bddd0
492397708 <492397708@qq.com>
2022-09-08 19:47:01 +0800

    稍微重置了一下项目 开始搭建框架

--- MCServer/src/Main/Class/Entity/baseEntity.java
+++
@@ -1,8 +0,0 @@
-package Main.Class.Entity;
-
-import java.util.HashMap;
-
-public class baseEntity {
-    public String name;
-    public HashMap<String, Object> values;
-}
--- MCServer/src/Main/Main.java
+++ MCServer/src/Main/Main.java
@@ -1,12 +1,7 @@
 package Main;
 
-import Main.Class.Events.Event;
-import Main.Data.StaticData;
 import Main.Utils.InitUtil;
 
-import net.mamoe.mirai.Bot;
-import net.mamoe.mirai.event.events.FriendMessageEvent;
-import net.mamoe.mirai.event.events.GroupMessageEvent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -20,35 +15,14 @@ import java.io.IOException;
 @SpringBootApplication
 public class Main {
 
-    public static StaticData staticData = new StaticData();
 
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
         SpringApplication.run(Main.class, args);
-//        staticData.bot.login();
-//        afterLogin(staticData.bot);
         InitUtil.init();
 //        SpringApplication.run(Main.class, args);
 
 
     }
 
-    public static void afterLogin(Bot bot) {
-
-
-        //好友信息
-        bot.getEventChannel().subscribeAlways(FriendMessageEvent.class, (event) -> {
-
-        });
-        bot.getEventChannel().subscribeAlways(GroupMessageEvent.class, (event) -> {
-
-            for (Event e : staticData.perMissionData.events.values()
-            ) {
-                e.passEvent(event.getMessage().contentToString(), event.getGroup(), event.getSender());
-            }
-
-        });
-
-
-    }
 }
