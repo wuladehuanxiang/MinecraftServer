61b3e21cb40cbb743e769714dfa4dd2cbcad93e2
492397708 <492397708@qq.com>
2022-10-04 19:05:18 +0800

    2022.10.4 书写大量前端结构

--- MCServerWeb/src/router.js
+++ MCServerWeb/src/router.js
@@ -1,11 +1,13 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 import MainPage from './components/ComonComponents/Main/MainPage.vue'
-import Item from './components/ComonComponents/ItemCompent/Item.vue'
-import LoginPage1 from './components/ComonComponents/Headers/Login/LoginPage1.vue'
-import LoginCard1 from './components/ComonComponents/Headers/Login/LoginCards/LoginAndRegist1.vue'
-import LoginCard2 from './components/ComonComponents/Headers/Login/LoginCards/LoginAndRegist2.vue'
-import Table from './components/ComonComponents/Table/Table.vue'
+import Home from "./components/ComonComponents/Content/Home.vue";
+import Atlas from "./components/ComonComponents/Content/Atlas.vue";
+import Donate from "./components/ComonComponents/Content/Donate.vue";
+import Forum from "./components/ComonComponents/Content/Forum.vue";
+import Rules from "./components/ComonComponents/Content/Rules.vue";
+import Server from "./components/ComonComponents/Content/Server.vue";
+import LoginPage1 from "./components/ComonComponents/Headers/Login/LoginPage1.vue"
 
 
 Vue.use(VueRouter);
